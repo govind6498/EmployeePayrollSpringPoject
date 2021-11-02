11bbadc94a4ea73801fd97a9dfcdd25cbf706289
govind6498 <govindsingh6498@gmail.com>
2021-11-02 23:20:26 +0530

    Added:Store List,upadate list to crud operation

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.employeepayrollapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+@Service
+public class EmployeePayrollService implements IEmployeePayrollService{
+
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+	@Override
+	public List<EmployeePayrollData> getEmployeePayrollData() {
+		return employeePayrollList;
+	}
+
+	@Override
+	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+		return employeePayrollList.get(empId-1);
+	}
+
+	@Override
+	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData employeePayrollData = null;
+		employeePayrollData = new EmployeePayrollData(1,empPayrollDTO);
+		employeePayrollList.add(employeePayrollData);
+		return employeePayrollData;
+	}
+
+	@Override
+	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
+		empData.setName(empPayrollDTO.name);
+		empData.setSalary(empPayrollDTO.salary);
+		employeePayrollList.set(empId-1, empData);
+		return empData;
+	}
+
+	@Override
+	public void deleteEmployeePayrollData(int empId) {
+		employeePayrollList.remove(empId-1);
+		
+	}
+
+}
