a7234b23a8ea2caee8de636ab4c9edaf7b1170ee
govind6498 <govindsingh6498@gmail.com>
2021-11-08 07:42:29 +0530

    Added:Remaining properties to employee payroll model

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,11 +1,23 @@
 package com.bridgelabz.employeepayrollapp.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
-public class EmployeePayrollData {
+
+import lombok.Data;
+
+public @Data class EmployeePayrollData {
 	private int employeeId;
 	private String name;
 	private long salary;
+	private String gender;
+	private LocalDate startDate;
+	private String note;
+	private String profilePic;
+	private List<String>department;
+	
 
 	public EmployeePayrollData() {}
 
@@ -13,24 +25,10 @@ public class EmployeePayrollData {
 		this.employeeId = empId;
 		this.name= empPayrollDTO.name;
 		this.salary = empPayrollDTO.salary;
-	}
-
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getSalary() {
-		return salary;
-	}
-	public void setSalary(long salary) {
-		this.salary = salary;
+		this.gender = empPayrollDTO.gender;
+		this.note = empPayrollDTO.note;
+		this.startDate = empPayrollDTO.startDate;
+		this.profilePic = empPayrollDTO.profilePic;
+		this.department = empPayrollDTO.department;
 	}
 }
