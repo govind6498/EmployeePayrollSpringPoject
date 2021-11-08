dbe3ea6b5f8dfb8b67f6847b84c1aef24ca8e7b1
govind6498 <govindsingh6498@gmail.com>
2021-11-08 22:18:27 +0530

    Added:To Create ebtities from properties in the model

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,14 +1,26 @@
 package com.bridgelabz.employeepayrollapp.model;
-
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
-
+import javax.persistence.JoinColumn;
 
 import lombok.Data;
-
+@Entity
+@Table(name="employee_payroll")
 public @Data class EmployeePayrollData {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="employee_id")
 	private int employeeId;
 	private String name;
 	private long salary;
@@ -16,13 +28,17 @@ public @Data class EmployeePayrollData {
 	private LocalDate startDate;
 	private String note;
 	private String profilePic;
-	private List<String>department;
 	
-
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name="department")
+	private List<String>department;
 	public EmployeePayrollData() {}
-
-	public EmployeePayrollData(int empId,EmployeePayrollDTO empPayrollDTO) {
-		this.employeeId = empId;
+	
+	public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		this.updateEmployePayrollData(empPayrollDTO);
+	}
+	private void updateEmployePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		this.name= empPayrollDTO.name;
 		this.salary = empPayrollDTO.salary;
 		this.gender = empPayrollDTO.gender;
@@ -30,5 +46,6 @@ public @Data class EmployeePayrollData {
 		this.startDate = empPayrollDTO.startDate;
 		this.profilePic = empPayrollDTO.profilePic;
 		this.department = empPayrollDTO.department;
+		
 	}
 }
